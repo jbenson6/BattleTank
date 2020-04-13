0e3eb379b2658c7dc69883fc5bb12a36a74b42b2
jbenson6 <jdbenson9@hotmail.com>
2020-04-12 20:24:13 -0500

    Setting Up RayCasting To Reticle

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -32,6 +32,20 @@ void ATankPlayerController::Tick(float DeltaTime)
 void ATankPlayerController::AimTowardsCrosshair()
 {
 	if(!GetControlledTank()){return;}
+
+	FVector HitLocation; // Out parameter
+	//UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+	if(GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())
+	}
 	
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	HitLocation = FVector(1.f);
+	
+	return true;
 }
 
